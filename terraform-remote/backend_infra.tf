b45358cf30c55c8dd87a207a53656065209cec53
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-10-17 13:34:50 +0530

    Create backend_infra.tf

---
+++ terraform-remote/backend_infra.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.16.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAQFJRRY34SV37IUVV"
+    secret_key = "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"
+
+}
+
+resource "aws_s3_bucket" "backend_bucket" {
+    bucket = "4remotebackendbucket"
+}
+
+resource "aws_dynamodb_table" "backend_table" {
+    name = "4remotebackenddb"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
